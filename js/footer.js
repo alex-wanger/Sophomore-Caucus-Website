df3f72631892c3b6c37260a99ad128a7ba5aed5e
Awang70 <alexwang94834@gmail.com>
2024-11-28 02:23:09 -0500

    change to pink and introduce more modular css

---
+++ js/footer.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header").innerHTML = data;
+        });
+});
+
+
+
